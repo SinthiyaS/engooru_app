1253e789c3da09321b0489593f145cf9d0d1f6a5
sinthiya <sinthiyasaravanan2205@gmail.com>
2021-11-17 17:16:09 +0530

    engooru

---
+++ backend/data.js
@@ -0,0 +1,81 @@
+import bcrypt from 'bcryptjs';
+const data = {
+    users: [
+        {
+          name: 'Sinthiya',
+          email: 'admin@example.com',
+          password: bcrypt.hashSync('1234', 8),
+          isAdmin: true,
+        },
+        {
+          name: 'Sruthi',
+          email: 'user@example.com',
+          password: bcrypt.hashSync('1234', 8),
+          isAdmin: false,
+        },
+      ],
+    products:[
+        {
+            name:'Onion Pallari',
+            category: 'Vegetables',
+            image: '/images/p1.jpg',
+            price: 25,
+            countInStock: 10,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Good Quality Product',
+        },
+        {
+            name:'Tomato',
+            category: 'Vegetables',
+            image: '/images/p2.jpg',
+            price: 15,
+            countInStock: 20,
+            rating: 4.0,
+            numReviews: 15,
+            description: 'Good Quality Product',
+        },
+        {
+            name:'Potato',
+            category: 'Vegetables',
+            image: '/images/p3.jpg',
+            price: 20,
+            countInStock: 25,
+            rating: 4.5,
+            numReviews: 20,
+            description: 'Good Quality Product',
+        },
+        {
+            name:'Cabbage',
+            category: 'Vegetables',
+            image: '/images/p4.jpg',
+            price: 15,
+            countInStock: 15,
+            rating: 4.5,
+            numReviews: 7,
+            description: 'Good Quality Product',
+        },
+        {
+            name:'Ladies Finger',
+            category: 'Vegetables',
+            image: '/images/p5.jpg',
+            price: 18,
+            countInStock: 0,
+            rating: 4.8,
+            numReviews: 2,
+            description: 'Good Quality Product',
+        },
+        {
+            name:'Carrot',
+            category: 'Vegetables',
+            image: '/images/p6.jpg',
+            price: 30,
+            countInStock: 20,
+            rating: 4.5,
+            numReviews: 14,
+            description: 'Good Quality Product',
+        },
+    ],
+};
+
+export default data;
